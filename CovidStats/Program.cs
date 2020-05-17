f5c2be5bb342a282613747d43efb05c58ab30ce9
Bence Makkos <b.makkos@teamsystem.com>
2020-05-17 18:54:18 +0200

    init

---
+++ CovidStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CovidStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
